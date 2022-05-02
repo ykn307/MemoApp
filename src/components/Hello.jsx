d9ceffbcd9b108014e3c096180623db146ae1550
ykn307 <69953230+ykn307@users.noreply.github.com>
2022-05-02 09:55:16 +0900

    Create Hello component

---
+++ src/components/Hello.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import {View, StyleSheet,Text} from "react-native";
+import {string,bool,shape} from "prop-types";
+
+const Hello = (props) => {
+  const {children, bang, style} = props;
+  return (
+    <View>
+      <Text style={[styles.text, style]}>
+        {`Hello ${children}${bang ? "!" : ""}`}
+      </Text>
+    </View>
+  );
+}
+
+Hello.propTypes = {
+  children: string.isRequired,
+  bang:bool,
+  style:shape(),
+};
+
+Hello.defaultProps = {
+  bang: false,
+  style: null,
+};
+
+const styles = StyleSheet.create({
+  text:{
+    color: "#ffffff",
+    backgroundColor:"blue",
+    fontSize:40,
+    fontWeight: "bold",
+    padding: 16,
+  },
+});
+
+export default Hello;
