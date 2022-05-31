1a80c413ae74b5c96f15ecb4c555a94f06c0db4d
ykn307 <69953230+ykn307@users.noreply.github.com>
2022-05-31 16:16:00 +0900

    Create Login Screen

---
+++ src/components/Button.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {View, Text, StyleSheet} from "react-native";
+import {string} from "prop-types";
+
+
+const Button = (props) => {
+  const {label} = props;
+  return (
+    <View style={styles.buttonContainer} >
+     <Text style={styles.buttonLabel}>{label}</Text>
+    </View>
+  );
+}
+
+Button.propTypes = {
+  label: string.isRequired,
+}
+
+
+const styles = StyleSheet.create({
+  buttonContainer:{
+    backgroundColor:"#467FD3",
+    borderRadius:4,
+    alignSelf: "flex-start",
+    marginBottom:24,
+  },
+  buttonLabel:{
+    fontSize:16,
+    lineHeight:32,
+    paddingVertical:8,
+    paddingHorizontal: 32,
+    color:"#ffffff",
+  },
+});
+
+export default Button
+
