f46daa76c33d67e0d2d398fac044befc99a170f3
ykn307 <69953230+ykn307@users.noreply.github.com>
2022-06-18 18:15:14 +0900

    Fetch data from firestore and pass it to flatItem

--- src/components/MemoList.jsx
+++ src/components/MemoList.jsx
@@ -1,66 +1,54 @@
 import React from "react";
-import {View, Text, StyleSheet,TouchableOpacity, Alert} from "react-native";
+import {View, Text, StyleSheet,TouchableOpacity, Alert, FlatList} from "react-native";
 import {useNavigation} from "@react-navigation/native";
+import {shape, string, instanceOf, arrayOf} from "prop-types";
 
 
-const MemoList = () =>{
+const MemoList = (props) =>{
+  const {memos} = props;
   const navigation = useNavigation();
+
+const renderItem = ({item}) => {
+  return(
+    <TouchableOpacity
+        style={styles.memoListItem}
+        onPress={() =>{navigation.navigate("MemoDetail");}}
+        >
+          <View>
+          <Text style={styles.memoListItemTitle} numberOfLines={1}>{item.bodyText}</Text>
+            <Text style={styles.memoListItemDate}>{String(item.updateAt)}</Text>
+          </View>
+          <TouchableOpacity
+          style={styles.memoDelete}
+          onPress={() =>{Alert.alert("Are you sure?");}}
+          >
+            <Text>X</Text>
+          </TouchableOpacity>
+        </TouchableOpacity>
+  );
+
+}
+
   return (
       <View>
-        <TouchableOpacity
-        style={styles.memoListItem}
-        onPress={() =>{navigation.navigate("MemoDetail");}}
-        >
-          <View>
-            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-            <Text style={styles.memoListItemDate}>2020年12月24日 10:00</Text>
-          </View>
-          <TouchableOpacity
-          style={styles.memoDelete}
-          onPress={() =>{Alert.alert("Are you sure?");}}
-          >
-            <Text>X</Text>
-          </TouchableOpacity>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-        style={styles.memoListItem}
-        onPress={() =>{navigation.navigate("MemoDetail");}}
-        >
-          <View>
-            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-            <Text style={styles.memoListItemDate}>2020年12月24日 10:00</Text>
-          </View>
-          <TouchableOpacity
-          style={styles.memoDelete}
-          onPress={() =>{Alert.alert("Are you sure?");}}
-          >
-            <Text>X</Text>
-          </TouchableOpacity>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-        style={styles.memoListItem}
-        onPress={() =>{navigation.navigate("MemoDetail");}}
-        >
-          <View>
-            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-            <Text style={styles.memoListItemDate}>2020年12月24日 10:00</Text>
-          </View>
-          <TouchableOpacity
-          style={styles.memoDelete}
-          onPress={() =>{Alert.alert("Are you sure?");}}
-          >
-            <Text>X</Text>
-          </TouchableOpacity>
-        </TouchableOpacity>
-
-
+        <FlatList
+          data={memos}
+          renderItem={renderItem}
+          keyExtractor={(item) => {return item.id;}}
+          />
     </View>
 
 
   );
 }
+
+MemoList.propTypes = {
+  memos: arrayOf(shape({
+    id: string,
+    bodyText:string,
+    updateAt:instanceOf(Date),
+  })).isRequired,
+};
 
 const styles = StyleSheet.create({
   memoListItem: {
--- src/screens/MemoListScreen.jsx
+++ src/screens/MemoListScreen.jsx
@@ -1,5 +1,6 @@
-import React, {useEffect} from 'react';
-import {StyleSheet, View} from "react-native";
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, View, Alert} from "react-native";
+import firebase from "firebase";
 
 import MemoList from "../components/MemoList";
 import CircleButton from "../components/CircleButton";
@@ -8,14 +9,42 @@ import LogOutButton from "../components/LogOutButton";
 
 const MemoListScreen = (props) => {
   const {navigation } = props;
+  const [memos, setMemos] = useState([]);
   useEffect(() => {
   navigation.setOptions({
     headerRight:() => <LogOutButton/>,
   });
 }, []);
+
+useEffect(() => {
+  const db = firebase.firestore();
+  const {currentUser} = firebase.auth();
+  let unsubscribe = () => {};
+  if (currentUser) {
+    const ref= db.collection(`users/${currentUser.uid}/memos`).orderBy("updatedAt", "desc");
+  unsubscribe = ref.onSnapshot((snapshot) => {
+    const userMemos = [];
+
+    snapshot.forEach((doc) => {
+      console.log(doc.id, doc.data());
+      const data = doc.data();
+      userMemos.push({
+        id:doc.id,
+        bodyText:data.bodyText,
+        updateAt:Date(data.updateAt),
+      });
+    });
+    setMemos(userMemos);
+  },(error) => {
+    console.log(error);
+    Alert.alert("データの読み込みに失敗しました。");
+  });
+  }
+  return unsubscribe;
+},[]);
   return (
     <View style={styles.container}>
-      <MemoList/>
+      <MemoList memos={memos}/>
       <CircleButton
       onPress={() =>{navigation.navigate("MemoCreate");}}
       >+</CircleButton>
